cd88c0c9129cf7a2e17380d882204312dcf6b539
Anuroop <anuroop.suresh@gmail.com>
2019-11-26 18:47:05 +0530

    dummydata

---
+++ customUtils/dummyData.js
@@ -0,0 +1,373 @@
+module.exports = {
+    dummyUsers:[
+        {
+            "name": "subbu1",
+            "phone": 918073498189
+        },
+        {
+            "name": "subbu2",
+            "phone": 918073498190
+        },
+        {
+            "name": "subbu3",
+            "phone": 918073498191
+        },
+        {
+            "name": "subbu4",
+            "phone": 918073498192
+        },
+        {
+            "name": "subbu5",
+            "phone": 918073498193
+        },
+        {
+            "name": "subbu6",
+            "phone": 918073498194
+        },
+        {
+            "name": "subbu7",
+            "phone": 918073498195
+        },
+        {
+            "name": "subbu8",
+            "phone": 918073498196
+        },
+        {
+            "name": "subbu9",
+            "phone": 918073498197
+        },
+        {
+            "name": "subbu10",
+            "phone": 918073498198
+        },
+        {
+            "name": "subbu11",
+            "phone": 918073498199
+        },
+        {
+            "name": "subbu12",
+            "phone": 918073498200
+        },
+        {
+            "name": "subbu13",
+            "phone": 918073498201
+        },
+        {
+            "name": "subbu14",
+            "phone": 918073498202
+        },
+        {
+            "name": "subbu15",
+            "phone": 918073498203
+        },
+        {
+            "name": "subbu16",
+            "phone": 918073498204
+        },
+        {
+            "name": "subbu17",
+            "phone": 918073498205
+        },
+        {
+            "name": "subbu18",
+            "phone": 918073498206
+        },
+        {
+            "name": "subbu19",
+            "phone": 918073498207
+        },
+        {
+            "name": "subbu20",
+            "phone": 918073498208
+        },
+        {
+            "name": "subbu21",
+            "phone": 918073498209
+        },
+        {
+            "name": "subbu22",
+            "phone": 918073498210
+        },
+        {
+            "name": "subbu23",
+            "phone": 918073498211
+        },
+        {
+            "name": "subbu24",
+            "phone": 918073498212
+        },
+        {
+            "name": "subbu25",
+            "phone": 918073498213
+        },
+        {
+            "name": "subbu26",
+            "phone": 918073498214
+        },
+        {
+            "name": "subbu27",
+            "phone": 918073498215
+        },
+        {
+            "name": "subbu28",
+            "phone": 918073498216
+        },
+        {
+            "name": "subbu29",
+            "phone": 918073498217
+        },
+        {
+            "name": "subbu30",
+            "phone": 918073498218
+        },
+        {
+            "name": "subbu31",
+            "phone": 918073498219
+        },
+        {
+            "name": "subbu32",
+            "phone": 918073498220
+        },
+        {
+            "name": "subbu33",
+            "phone": 918073498221
+        },
+        {
+            "name": "subbu34",
+            "phone": 918073498222
+        },
+        {
+            "name": "subbu35",
+            "phone": 918073498223
+        },
+        {
+            "name": "subbu36",
+            "phone": 918073498224
+        },
+        {
+            "name": "subbu37",
+            "phone": 918073498225
+        },
+        {
+            "name": "subbu38",
+            "phone": 918073498226
+        },
+        {
+            "name": "subbu39",
+            "phone": 918073498227
+        },
+        {
+            "name": "subbu40",
+            "phone": 918073498228
+        },
+        {
+            "name": "subbu41",
+            "phone": 918073498229
+        },
+        {
+            "name": "subbu42",
+            "phone": 918073498230
+        },
+        {
+            "name": "subbu43",
+            "phone": 918073498231
+        },
+        {
+            "name": "subbu44",
+            "phone": 918073498232
+        },
+        {
+            "name": "subbu45",
+            "phone": 918073498233
+        },
+        {
+            "name": "subbu46",
+            "phone": 918073498234
+        },
+        {
+            "name": "subbu47",
+            "phone": 918073498235
+        },
+        {
+            "name": "subbu48",
+            "phone": 918073498236
+        },
+        {
+            "name": "subbu49",
+            "phone": 918073498237
+        },
+        {
+            "name": "subbu50",
+            "phone": 918073498238
+        },
+        {
+            "name": "subbu51",
+            "phone": 918073498239
+        },
+        {
+            "name": "subbu52",
+            "phone": 918073498240
+        },
+        {
+            "name": "subbu53",
+            "phone": 918073498241
+        },
+        {
+            "name": "subbu54",
+            "phone": 918073498242
+        },
+        {
+            "name": "subbu55",
+            "phone": 918073498243
+        },
+        {
+            "name": "subbu56",
+            "phone": 918073498244
+        },
+        {
+            "name": "subbu57",
+            "phone": 918073498245
+        },
+        {
+            "name": "subbu58",
+            "phone": 918073498246
+        },
+        {
+            "name": "subbu59",
+            "phone": 918073498247
+        },
+        {
+            "name": "subbu60",
+            "phone": 918073498248
+        },
+        {
+            "name": "subbu61",
+            "phone": 918073498249
+        },
+        {
+            "name": "subbu62",
+            "phone": 918073498250
+        },
+        {
+            "name": "subbu63",
+            "phone": 918073498251
+        },
+        {
+            "name": "subbu64",
+            "phone": 918073498252
+        },
+        {
+            "name": "subbu65",
+            "phone": 918073498253
+        },
+        {
+            "name": "subbu66",
+            "phone": 918073498254
+        },
+        {
+            "name": "subbu67",
+            "phone": 918073498255
+        },
+        {
+            "name": "subbu68",
+            "phone": 918073498256
+        },
+        {
+            "name": "subbu69",
+            "phone": 918073498257
+        },
+        {
+            "name": "subbu70",
+            "phone": 918073498258
+        },
+        {
+            "name": "subbu71",
+            "phone": 918073498259
+        },
+        {
+            "name": "subbu72",
+            "phone": 918073498260
+        },
+        {
+            "name": "subbu73",
+            "phone": 918073498261
+        },
+        {
+            "name": "subbu74",
+            "phone": 918073498262
+        },
+        {
+            "name": "subbu75",
+            "phone": 918073498263
+        },
+        {
+            "name": "subbu76",
+            "phone": 918073498264
+        },
+        {
+            "name": "subbu77",
+            "phone": 918073498265
+        },
+        {
+            "name": "subbu78",
+            "phone": 918073498266
+        },
+        {
+            "name": "subbu79",
+            "phone": 918073498267
+        },
+        {
+            "name": "subbu80",
+            "phone": 918073498268
+        },
+        {
+            "name": "subbu81",
+            "phone": 918073498269
+        },
+        {
+            "name": "subbu82",
+            "phone": 918073498270
+        },
+        {
+            "name": "subbu83",
+            "phone": 918073498271
+        },
+        {
+            "name": "subbu84",
+            "phone": 918073498272
+        },
+        {
+            "name": "subbu85",
+            "phone": 918073498273
+        },
+        {
+            "name": "subbu86",
+            "phone": 918073498274
+        },
+        {
+            "name": "subbu87",
+            "phone": 918073498275
+        },
+        {
+            "name": "subbu88",
+            "phone": 918073498276
+        },
+        {
+            "name": "subbu89",
+            "phone": 918073498277
+        },
+        {
+            "name": "subbu90",
+            "phone": 918073498278
+        },
+        {
+            "name": "subbu91",
+            "phone": 918073498279
+        },
+        {
+            "name": "subbu92",
+            "phone": 918073498188
+        }
+    ],
+
+};
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const router = express.Router();
 const {nativePostgresPool, knexPool, vNeoAppAuth, vNeoAppDb} = require('../config/dbConfig');
+const dummyData = require('../customUtils/dummyData');
 
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
+
+router.post('/User', async function (req, res) {
+    let client;
+    try {
+        client = await nativePostgresPool.connect();
+
+        const dummyUser = dummyData.dummyUsers;
+
+        /*const reqObject = req.body;
+
+        const queryStr = `INSERT INTO "User" ("name","phone") VALUES ('${reqObject.name}','${reqObject.phone}') RETURNING *;`;
+        const queryResult = await client.query(queryStr);*/
+        let queryStr, queryResult;
+        for (let user of dummyUser) {
+            queryStr = `INSERT INTO "User" ("name","phone") VALUES ('${user.name}','${user.phone}') RETURNING *;`;
+            queryResult = await client.query(queryStr);
+        }
+        //console.log('index./user: ', queryResult);
+        res.send(queryResult.rows)
+
+    } catch (e) {
+        console.error('index./channel: ', e);
+        res.status(400).send("Bad Request " + e.message)
+    } finally {
+        client.release();
+    }
+});
+
 
 router.post('/Channel', async function (req, res) {
     let client;
@@ -27,27 +56,6 @@ router.post('/Channel', async function (req, res) {
     }
 });
 
-router.post('/User', async function (req, res) {
-    let client;
-    try {
-        client = await nativePostgresPool.connect();
-
-        const reqObject = req.body;
-
-        const queryStr = `INSERT INTO "User" ("name","phone") VALUES ('${reqObject.name}','${reqObject.phone}');`;
-        const queryResult = await client.query(queryStr);
-
-        console.log('index./user: ', queryResult);
-        res.send(queryResult)
-
-    } catch (e) {
-        console.error('index./channel: ', e);
-        res.status(400).send("Bad Request " + e.message)
-    } finally {
-        client.release();
-    }
-});
-
 router.post('/invite', async function (req, res) {
     let client;
     try {
@@ -55,7 +63,7 @@ router.post('/invite', async function (req, res) {
 
         const reqObject = req.body;
 
-        const queryStr = `INSERT INTO "Channel" ("name") VALUES ('${reqObject.name}');`;
+        const queryStr = `INSERT INTO "Channel" ("name") VALUES ('${reqObject.name}') RETURNING *;`;
         const queryResult = await client.query(queryStr);
         res.send(queryResult)
 
@@ -72,13 +80,30 @@ router.post('/publishMoyoToUser', async function (req, res) {
     try {
         client = await nativePostgresPool.connect();
 
-        const moyoObj = req.body.moyo;
+        let moyoList = req.body;
+        for(let moyoObj of moyoList){
 
-        const queryStr = `INSERT INTO "Moyo" ("name","description","channelId","status") 
-        VALUES ('${moyoObj.name}','${moyoObj.description}','${moyoObj.channelID}','${moyoObj.status}');`;
-        const queryResult = await client.query(queryStr);
-        console.log('index.queryResult: ',);
-        res.send(queryResult)
+            let queryStr = `INSERT INTO "Moyo" ("name","description","channelId","status") 
+        VALUES ('${moyoObj.name}','${moyoObj.description}','${moyoObj.channelID}','${moyoObj.status}') RETURNING *;`;
+            let queryResult = await client.query(queryStr);
+            const insertedMoyoRow = queryResult.rows[0];
+
+            // get user id by phone number
+            let userObj;
+            for (let phone of moyoObj.Userlist) {
+                queryStr = `SELECT * from "User" where "phone" like '${phone}'`;
+                queryResult = await client.query(queryStr);
+                userObj = queryResult.rows[0];
+                if (userObj) {
+                    queryStr = `INSERT INTO "USER_MOYO_JUNCTION" ("User","Moyo") VALUES (${userObj.id},${insertedMoyoRow.id})`;
+                } else {
+                    throw new Error('No User exist for phone ' + phone);
+                }
+            }
+        }
+
+
+        res.send(insertedMoyoRow)
 
     } catch (e) {
         console.error('index./channel: ', e);
@@ -95,6 +120,9 @@ router.post('/getMoyosOfUsers', async function (req, res) {
 
         const reqObject = req.body;
 
+        for (let phone of reqObject.users) {
+
+        }
         const queryStr = `INSERT INTO "Channel" ("name") VALUES ('${reqObject.name}');`;
         const queryResult = await client.query(queryStr);
         res.send(queryResult)
